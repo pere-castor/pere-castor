2fba9a7962391f371d41df03bedba005fff2531a
archer <archer.tb@hotmail.fr>
2014-06-26 12:50:33 +0200

    premier commit

---
+++ website/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pere Castor - un geek pas comme les autres</title>
+    </head>
+    <body>
+
+    </body>
+</html>
